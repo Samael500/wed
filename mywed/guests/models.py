2f5fadb535435386e2b83d6a56486153235f8567
Samael500 <samael500@gmail.com>
2015-02-25 19:12:21 +0300

    :rocket: admin guest inline

--- mywed/guests/models.py
+++ mywed/guests/models.py
@@ -7,7 +7,7 @@ class Guest(models.Model):
 
     """ My wedding guest's """
 
-    user = models.ForeignKey(User, verbose_name='Пользователь', blank=True, null=True)
+    user = models.ForeignKey(User, verbose_name='Пользователь', blank=True, null=True, related_name='guest_profile')
     user_key = models.CharField(verbose_name='Код доступа', max_length=8, unique=True, validators=[RegexValidator(
         '^[\w\d]{8}$', message='Код доступа должен состоять из 8 символов (английские буквы или цифры).')])
 
@@ -15,7 +15,14 @@ class Guest(models.Model):
         """ create user on save """
         if not self.pk and self.user is None:
             self.user = User.objects.create_user(username=self.user_key, password=self.user_key)
+        else:
+            self.user.username = self.user_key
+            self.user.set_password(self.user_key)
+            self.user.save()
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.user_key
 
     class Meta:
         verbose_name='Гость'
