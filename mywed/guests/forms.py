76ef10cb8d00c8641a25e49752b6c3734060062c
Maks <samael500@gmail.com>
2015-12-09 17:27:36 +0300

    :repeat: set max length for code login

--- mywed/guests/forms.py
+++ mywed/guests/forms.py
@@ -8,12 +8,14 @@ class LoginForm(forms.Form):
 
     """ Form for login guests """
 
-    user_key = forms.CharField(required=True, label=Guest._meta.get_field_by_name('user_key')[0].verbose_name)
+    user_key = forms.CharField(
+        required=True, max_length=8, label=Guest._meta.get_field_by_name('user_key')[0].verbose_name)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._set_errors_css()
         self.fields['user_key'].widget.attrs['placeholder'] = self.fields['user_key'].label
+        self.fields['user_key'].widget.attrs['max_length'] = 8
 
     def _set_errors_css(self):
         for field in self:
